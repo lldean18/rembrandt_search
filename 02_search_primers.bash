b4fe706cae47fa6a7e2d40ecd25517ab4fdc885b
Laura Dean <lldean18@gmail.com>
2025-06-19 16:25:01 +0100

    first attempt at nested for loop to run over all the cell lines

--- 02_search_primers.bash
+++ 02_search_primers.bash
@@ -9,14 +9,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10g
-#SBATCH --time=4:00:00
+#SBATCH --time=2:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # set variables
 wkdir=/gpfs01/home/mbzlld/data/rembrandt
 reads1=$wkdir/SRR8615649_1.fastq.gz
-reads2=$wkdir/SRR8615649_2.fastq.gz
-primers=$wkdir/KCNMA1_STREX_primers.fa
+cell_lines=$wkdir/cell_lines.txt
 
 
 # Install and load software
@@ -36,25 +35,39 @@ K=21
 HDIST=1
 mkh=1
 
-# map the reads to primers
-bbduk.sh \
--Xmx2048M \
-in=$reads1 \
-in2=$reads2 \
-k=$K \
-hdist=$HDIST \
-mkh=$mkh \
-out=$wkdir/$(basename "${reads1%_*}")_without_primers_k${K}_hdist${HDIST}_mkh${mkh}.fastq \
-outm=$wkdir/$(basename "${reads1%_*}")_with_primers_k${K}_hdist${HDIST}_mkh${mkh}.fastq \
-stats=$wkdir/$(basename "${reads1%_*}")_stats_k${K}_hdist${HDIST}_mkh${mkh}.txt \
-ref=$primers
+
+# create an array of read files to loop over
+accessions=($(tail -n +2 $cell_lines | cut -f2))
+reads=( "${accessions[@]/#/$wkdir\/}" )
+reads=( "${reads[@]/%/_1.fastq.gz}" )
 
 
-# convert the real matches to fasta format
-seqtk seq -A $wkdir/$(basename "${reads1%_*}")_with_primers_k${K}_hdist${HDIST}_mkh${mkh}.fastq > $wkdir/$(basename "${reads1%_*}")_reads_with_primers.fasta
-
-# remove unnecessary files
-rm $wkdir/$(basename "${reads1%_*}")_without_primers_k${K}_hdist${HDIST}_mkh${mkh}.fastq $wkdir/$(basename "${reads1%_*}")_with_primers_k${K}_hdist${HDIST}_mkh${mkh}.fastq
+# create an array containing the primer files to loop over
+primers=($wkdir/KCNMA1_STREX_primers.fa $wkdir/KCNMA1_primers.fa)
 
 
+
+for file in "${reads[@]}" ; do
+	for primer in "${primers[@]}" ; do
+		# map the reads to primers
+		bbduk.sh \
+		-Xmx2048M \
+		in=$file \
+		k=$K \
+		hdist=$HDIST \
+		mkh=$mkh \
+		out=$wkdir/$(basename "${file%.*.*}")_without_primers_$(basename "${primer%_*}").fastq \
+		outm=$wkdir/$(basename "${file%.*.*}")_with_primers_$(basename "${primer%_*}").fastq \
+		stats=$wkdir/$(basename "${file%.*.*}")_$(basename "${primer%_*}")_stats.txt \
+		ref=$primer
+		
+		# convert the real matches to fasta format
+		#seqtk seq -A $wkdir/$(basename "${file%.*.**}")_with_primers_$(basename "${primer%_*}").fastq > $wkdir/$(basename "${file%.*.*}")_reads_with_primers_$(basename "${primer%_*}").fasta
+		
+		# remove unnecessary files
+		rm $wkdir/$(basename "${file%.*.*}")_without_primers_$(basename "${primer%_*}").fastq
+		rm $wkdir/$(basename "${file%.*.*}")_with_primers_$(basename "${primer%_*}").fastq
+		
+	done
+done
 
