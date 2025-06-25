8afbe6877b6bcb6e1d9618fb72aba72e53080ecf
Laura Dean <lldean18@gmail.com>
2025-06-25 14:31:06 +0100

    adding more time since I downloaded more files to run on

--- 02_search_primers.bash
+++ 02_search_primers.bash
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10g
-#SBATCH --time=4:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # set variables
